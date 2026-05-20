8c31acf82dfdd079d79562a605f41f0729b9207f
vishal326323-lab <vishal326323@gmail.com>
2026-05-20 12:16:41 +0530

    Commit
    Set up an Express server to handle SMS sending via Twilio.

--- server.js
+++ server.js
@@ -1,1 +1,61 @@
 
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const twilio = require("twilio");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const accountSid = "AC6c7a74015f91b1be2575b0cce703bf3f";
+const authToken = "baa81c5da1da77551187b628f6d46a07";
+
+const client = twilio(accountSid, authToken);
+
+app.post("/send-sms", async (req, res) => {
+
+    const { name, email, message } = req.body;
+
+    try {
+
+        await client.messages.create({
+
+            body:
+`New Portfolio Message
+
+Name: ${name}
+
+Email: ${email}
+
+Message:
+${message}`,
+
+            from: "+19068286399",
+
+            to: "+919490326323"
+
+        });
+
+        res.status(200).json({
+            success: true
+        });
+
+    } catch (error) {
+
+        console.log(error);
+
+        res.status(500).json({
+            success: false
+        });
+
+    }
+
+});
+
+app.listen(5000, () => {
+
+    console.log("Server running on port 5000");
+
+});
